7ee18724d555375f525f294c16b12e02f242b908
vedadsar <vedadsa@windowslive.com>
2015-04-04 14:48:05 +0200

    added new dependency for Jsoup, adding coupon name and price in html

--- app/helpers/MailHelper.java
+++ app/helpers/MailHelper.java
@@ -2,8 +2,20 @@ package helpers;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.List;
 import java.util.Scanner;
+
+
+
+
+
+
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
 
 
 import models.Coupon;
@@ -78,8 +90,13 @@ public class MailHelper {
 		
 	}
 	
-	public static void sendNewsletter(List<String> emails, String subject, List<Coupon> coupons) {
-		
+	/**
+	 *Helper which sends newsletter email. 
+	 * @param emails
+	 * @param subject
+	 * @param coupons
+	 */
+	public static void sendNewsletter(List<String> emails, String subject, List<Coupon> coupons)  {
 		copyImages(coupons);
 		
 		Email mail = new Email();
@@ -103,17 +120,50 @@ public class MailHelper {
 			Logger.error("COULD'T READ EMAIL FILE");
 		}finally{
 			sc.close();
-		}	
+		}			
+
+		String preparedHTML = getPreparedHTML(message, coupons);		
 		
-		message.replace("CouponOneName", coupons.get(0).name);
-		
-		Logger.debug(message);
-		mail.setBodyHtml(message);
+	
+		mail.setBodyHtml(preparedHTML);
 		MailerPlugin.send(mail);
 
 	}	
 	
-	public static void copyImages(List<Coupon> coupons) {
+	/**
+	 * Method gets html template, and puts coupons info in it.
+	 * @param html
+	 * @param coupons
+	 * @return
+	 */
+	private static String getPreparedHTML(String html, List<Coupon> coupons){
+		Document doc =  Jsoup.parse(html);
+		
+		Element couponOneName = doc.getElementById("Cp1-name");
+		couponOneName.appendText(coupons.get(0).name);		
+		Element couponOnePrice = doc.getElementById("Cp1-price");
+		couponOnePrice.appendText(coupons.get(0).price +"€");
+		
+		Element couponTwoName = doc.getElementById("Cp2-name");
+		couponTwoName.appendText(coupons.get(1).name);		
+		Element couponTwoPrice = doc.getElementById("Cp2-price");
+		couponTwoPrice.appendText(coupons.get(1).price +"€");
+		
+		Element couponThreeName = doc.getElementById("Cp3-name");
+		couponThreeName.appendText(coupons.get(2).name);		
+		Element couponThreePrice = doc.getElementById("Cp3-price");
+		couponThreePrice.appendText(coupons.get(2).price +"€");
+		
+		return doc.toString();
+	}
+	
+	
+	/**
+	 * Make copies of images for email newsletter.
+	 * TODO: Less hardcode
+	 * @param coupons
+	 */
+	private static void copyImages(List<Coupon> coupons) {
 		String photoOne = "./public/" +coupons.get(0).picture;
 		String photoTwo = "./public/" +coupons.get(1).picture;
 		String photoThree = "./public/" +coupons.get(2).picture;
