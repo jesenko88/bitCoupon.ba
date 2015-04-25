6f85b9a9d76a888b17ee913430b6467d3604a435
jesenko <jeshko2@hotmail.com>
2015-04-25 16:33:03 +0200

    add feature to buy gift for a unregistered user

--- app/controllers/UserController.java
+++ app/controllers/UserController.java
@@ -483,16 +483,21 @@ public class UserController extends Controller {
 	
 	// TODO comment
 	@Security.Authenticated(CurrentUserFilter.class)
-	public static Result giftCheckoutPage() {
+	public static Result giftCheckoutPage(long id) {
 		DynamicForm dynamicForm = Form.form().bindFromRequest();
-		long id = Long.parseLong(dynamicForm.data().get("coupon_id"));
 		Coupon coupon = Coupon.find(id);
 		String email = dynamicForm.data().get("email");
 		User user = User.findByEmail(email);
 		if (user == null) {
-			flash("error", "A user with email: " + email + " doesn't exist");
+			flash("info", "A user with email: " + email + " doesn't exist");
 			return badRequest(makeAGift.render(coupon));
 		}
 		return ok(buyForUser.render(coupon, user));
+	}
+	
+	@Security.Authenticated(CurrentUserFilter.class)
+	public static Result giftCheckoutPageUnregistered(long id) {
+		Coupon coupon = Coupon.find(id);
+		return ok(buyForUser.render(coupon, null));
 	}
 }
