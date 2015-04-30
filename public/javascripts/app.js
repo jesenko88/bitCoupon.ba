1843028cf8a4f00bdc424457815af68cbb1ad3fd
vedadsar <vedadsa@windowslive.com>
2015-05-01 00:58:37 +0200

    managed to connect angular with form

---
+++ public/javascripts/app.js
@@ -0,0 +1,17 @@
+// create angular app
+var validationApp = angular.module('movieApp', []);
+
+// create angular controller
+validationApp.controller('movieController', function($scope) {
+
+    // function to submit the form after all validation has occurred            
+    $scope.submitForm = function(isValid) {
+
+        // check to make sure the form is completely valid
+        if (isValid) { 
+            alert('our form is amazing');
+        }
+
+    };
+
+});
